937b56bbd9de4a808eaeabd48633121f0119c69e
Yulia Leonteva <mornemira@yandex.ru>
2018-10-19 20:40:18 +0300

    finish task

---
+++ src/ex5_js-functions/task.js
@@ -0,0 +1,53 @@
+var Calculator = Calc();
+
+function Calc(){
+    var result = 0;
+    
+    function add(){
+        if(arguments.length !== 0){
+            result += arguments[0];
+        }
+        return add;
+    }
+
+    function subtract(){
+        if(arguments.length !== 0){
+            result -= arguments[0];
+        }
+        return subtract;
+    }
+
+    function divide(){
+        if(arguments.length !== 0){
+            result /= arguments[0];
+        }
+        return divide;
+    }
+
+    function multiply(){
+        if(arguments.length !== 0){
+            result *= arguments[0];
+        }
+        return multiply;
+    }
+
+    function getResult(){
+        return result;
+    }
+
+    function reset(){
+        result = 0;
+        return reset;
+    }
+
+    return {
+        add: add,
+        subtract: subtract,
+        divide: divide,
+        multiply: multiply,
+        getResult: getResult,
+        reset: reset
+    }
+}
+
+
